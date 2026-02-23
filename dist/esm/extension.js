7f047fea46071cb465f248d641f8ee5d9cf46ef1
Theophilus Annor <annor_theophilus@hotmail.com>
2026-02-23 11:31:50 +0000

    fix: update RVM installation commands for consistency in Ruby setup

--- dist/esm/extension.js
+++ dist/esm/extension.js
@@ -30,8 +30,7 @@ class RubyExtension {
                 instructions: [
                     'RUN apt-get update && apt-get install -y curl gnupg2',
                     "RUN curl -sSL https://get.rvm.io | bash -s",
-                    `RUN rvm install ${rubyVersion} && rvm use ${rubyVersion} --default`,
-                    `RUN /bin/bash -l -c ". /etc/profile.d/rvm.sh && rvm install ${rubyVersion} && rvm use ${rubyVersion} --default"`,
+                    `RUN /bin/bash -l -c ". /etc/profile.d/rvm.sh && /etc/profile.d/rvm.sh install ${rubyVersion} && /etc/profile.d/rvm.sh use ${rubyVersion} --default"`,
                     "RUN gem install pg",
                     "RUN gem install rake",
                     ...(this.options.scripts?.map((script) => `RUN ${script}`) ?? []),
